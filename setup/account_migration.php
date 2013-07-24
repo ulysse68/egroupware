212e9449c0d6049be0c147b8f5c37f0debc2cdda
ulysse68 <ulysse68@gmail.com>
2013-07-24 14:50:57 +0200

    Original files taken with SVN

---
+++ setup/account_migration.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * eGroupware Setup - Account migration between SQL <--> LDAP
+ *
+ * The migration is done to the account-repository configured for eGroupWare!
+ *
+ * @link http://www.egroupware.org
+ * @package setup
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id: account_migration.php 42535 2013-05-23 14:36:52Z ralfbecker $
+ */
+
+include('./inc/functions.inc.php');
+
+// Authorize the user to use setup app and load the database
+if (!$GLOBALS['egw_setup']->auth('Config') || $_POST['cancel'])
+{
+	Header('Location: index.php');
+	exit;
+}
+// Does not return unless user is authorized
+
+$tpl_root = $GLOBALS['egw_setup']->html->setup_tpl_dir('setup');
+$setup_tpl = CreateObject('phpgwapi.Template',$tpl_root);
+$setup_tpl->set_file(array(
+	'migration' => 'account_migration.tpl',
+	'T_head' => 'head.tpl',
+	'T_footer' => 'footer.tpl',
+	'T_alert_msg' => 'msg_alert_msg.tpl'
+));
+
+// determine from where we migrate to what
+if (!is_object($GLOBALS['egw_setup']->db))
+{
+	$GLOBALS['egw_setup']->loaddb();
+}
+// Load configuration values account_repository and auth_type, as setup has not yet done so
+foreach($GLOBALS['egw_setup']->db->select($GLOBALS['egw_setup']->config_table,'config_name,config_value',
+	"config_name LIKE 'ldap%' OR config_name LIKE 'account_%' OR config_name LIKE '%encryption%' OR config_name='auth_type'",
+	__LINE__,__FILE__) as $row)
+{
+	$GLOBALS['egw_info']['server'][$row['config_name']] = $row['config_value'];
+}
+$from = $GLOBALS['egw_info']['server']['account_repository'];
+if (!$from && !($from = $GLOBALS['egw_info']['server']['auth_type']))
+{
+	$from = 'sql';
+}
+$to = $from == 'sql' ? 'ldap' : 'sql';
+$direction = strtoupper($from).' --> '.strtoupper($to);
+
+$GLOBALS['egw_setup']->html->show_header($direction,False,'config',$GLOBALS['egw_setup']->ConfigDomain .
+	'(' . $GLOBALS['egw_domain'][$GLOBALS['egw_setup']->ConfigDomain]['db_type'] . ')');
+
+// create base one level off ldap_context
+$base_parts = explode(',',$GLOBALS['egw_info']['server']['ldap_context']);
+array_shift($base_parts);
+
+$cmd = new setup_cmd_ldap(array(
+	'domain' => $GLOBALS['egw_setup']->ConfigDomain,
+	'sub_command' => 'migrate_to_'.$to,
+	// allow to set ldap root DN (ldap_admin) to create instance specific admin DN and structure
+	'ldap_admin' => !empty($_POST['ldap_admin']) ? $_POST['ldap_admin'] : $GLOBALS['egw_info']['server']['ldap_root_dn'],
+	'ldap_admin_pw' => !empty($_POST['ldap_admin']) ? $_POST['ldap_admin_pw'] : $GLOBALS['egw_info']['server']['ldap_root_pw'],
+	'ldap_base' => implode(',',$base_parts),
+	'truncate_egw_accounts' => !empty($_POST['truncate_egw_accounts']),
+)+$GLOBALS['egw_info']['server']);
+
+if (!$_POST['migrate'])
+{
+	$accounts = $cmd->accounts($from == 'ldap');
+
+	// now outputting the account selection
+	$setup_tpl->set_block('migration','header','header');
+	$setup_tpl->set_block('migration','user_list','user_list');
+	$setup_tpl->set_block('migration','group_list','group_list');
+	$setup_tpl->set_block('migration','ldap_admin','ldap_admin');
+	$setup_tpl->set_block('migration','submit','submit');
+	$setup_tpl->set_block('migration','footer','footer');
+	$setup_tpl->set_block('migration','truncate_egw_accounts','truncate_egw_accounts');
+
+	foreach($accounts as $account_id => $account)
+	{
+		if ($account['account_type'] == 'g')
+		{
+			if (isset($accounts[abs($account_id)]))
+			{
+				$identical_account_id_warning .= lang('Group %1 uses identical ID %2 as user %3!', $account['account_lid'], abs($account_id), $accounts[abs($account_id)]['account_lid']);
+			}
+			$group_list .= '<option value="' . $account_id . '" selected="1">'. $account['account_lid'] . "</option>\n";
+		}
+		else
+		{
+			$user_list .= '<option value="' . $account_id . '" selected="1">'.
+				$GLOBALS['egw']->common->display_fullname($account['account_lid'],
+				$account['account_firstname'],$account['account_lastname'])	. "</option>\n";
+		}
+	}
+	$setup_tpl->set_var('action_url','account_migration.php');
+	$setup_tpl->set_var('users',$user_list);
+	$setup_tpl->set_var('groups',$group_list);
+
+	$setup_tpl->set_var('description',lang('Migration between EGroupware account repositories').': '.$direction);
+	$setup_tpl->set_var('select_users',lang('Select which user(s) will be exported'));
+	$setup_tpl->set_var('select_groups',lang('Select which group(s) will be exported'));
+	$setup_tpl->set_var('memberships',$identical_account_id_warning ?
+		'<p style="color: red">'.$identical_account_id_warning.'<br/>'.lang('Migration of group will fail, as SQL does NOT allow identical IDs.').'</p>' :
+		lang('Group memberships will be migrated too.'));
+	$setup_tpl->set_var('ldap_admin_message', lang('Give LDAP root DN and password, if you need to create an instance specific admin user, user- or group-context'));
+	$setup_tpl->set_var('ldap_admin_label', lang('Root DN'));
+	$setup_tpl->set_var('ldap_admin_pw_label', lang('Root DN password'));
+	$setup_tpl->set_var('migrate',$direction);
+	$setup_tpl->set_var('cancel',lang('Cancel'));
+
+	$setup_tpl->pfp('out','header');
+	if($user_list)
+	{
+		$setup_tpl->pfp('out','user_list');
+	}
+	if($group_list)
+	{
+		$setup_tpl->pfp('out','group_list');
+	}
+	if ($to == 'ldap')
+	{
+		$setup_tpl->pfp('out','ldap_admin');
+	}
+	if ($to == 'sql')
+	{
+		$setup_tpl->set_var('truncate_egw_accounts_message', lang('Delete all existing accounts from SQL database'));
+		$setup_tpl->pfp('out','truncate_egw_accounts');
+	}
+	$setup_tpl->pfp('out','submit');
+	$setup_tpl->pfp('out','footer');
+}
+else	// do the migration
+{
+	$cmd->only = array_merge((array)$_POST['users'],(array)$_POST['groups']);
+	$cmd->verbose = true;
+	echo '<p align="center">'.str_replace("\n","</p>\n<p align='center'>",$cmd->run())."</p>\n";
+	// store new repostory (and auth_type), as we are migrated now
+	config::save_value('account_repository', $GLOBALS['egw_info']['server']['account_repository']=$to, 'phpgwapi');
+	if (empty($GLOBALS['egw_info']['server']['auth_type']) || $GLOBALS['egw_info']['server']['auth_type'] == $from)
+	{
+		config::save_value('auth_type', $GLOBALS['egw_info']['server']['auth_type']=$to, 'phpgwapi');
+	}
+	echo '<p align="center">'.lang('Click <a href="index.php">here</a> to return to setup.')."</p>\n";
+}
+
+$GLOBALS['egw_setup']->html->show_footer();
---
+++ setup/inc/functions.inc.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Setup
+ *
+ * @link http://www.egroupware.org
+ * @package setup
+ * @author Joseph Engo <jengo@phpgroupware.org>
+ * @author Dan Kuykendall <seek3r@phpgroupware.org>
+ * @author Mark Peters <skeeter@phpgroupware.org>
+ * @author Miles Lott <milos@groupwhere.org>
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id: functions.inc.php 42960 2013-07-04 15:42:13Z hnategh $
+ */
+
+error_reporting(error_reporting() & ~E_NOTICE & ~E_STRICT);
+
+// for an old header, we need to setup the reference before including it
+$GLOBALS['phpgw_info'] =& $GLOBALS['egw_info'];
+
+$GLOBALS['egw_info'] = array(
+	'flags' => array(
+		'noheader' => True,
+		'nonavbar' => True,
+		'currentapp' => 'setup',
+		'noapi' => True
+));
+if(file_exists('../header.inc.php'))
+{
+	include('../header.inc.php');
+}
+// for an old header we need to setup a reference for the domains
+if (!is_array($GLOBALS['egw_domain'])) $GLOBALS['egw_domain'] =& $GLOBALS['phpgw_domain'];
+
+if (!function_exists('version_compare'))//version_compare() is only available in PHP4.1+
+{
+	echo 'eGroupWare now requires PHP 4.1 or greater.<br>';
+	echo 'Please contact your System Administrator';
+	exit;
+}
+
+/*  If we included the header.inc.php, but it is somehow broken, cover ourselves... */
+if(!defined('EGW_SERVER_ROOT') && !defined('EGW_INCLUDE_ROOT'))
+{
+	if (defined('PHPGW_SERVER_ROOT') && defined('PHPGW_INCLUDE_ROOT'))	// pre 1.2 install
+	{
+		define('EGW_SERVER_ROOT',PHPGW_SERVER_ROOT);
+		define('EGW_INCLUDE_ROOT',PHPGW_INCLUDE_ROOT);
+	}
+	else	// no install
+	{
+		define('EGW_SERVER_ROOT','..');
+		define('EGW_INCLUDE_ROOT','..');
+		define('PHPGW_SERVER_ROOT','..');
+		define('PHPGW_INCLUDE_ROOT','..');
+	}
+	define('EGW_API_INC',EGW_SERVER_ROOT.'/phpgwapi/inc');
+}
+
+require_once(EGW_INCLUDE_ROOT . '/phpgwapi/inc/common_functions.inc.php');
+
+// check if eGW's pear repository is installed and prefer it over the regular one
+if (is_dir(EGW_SERVER_ROOT.'/egw-pear'))
+{
+	set_include_path(EGW_SERVER_ROOT.'/egw-pear'.PATH_SEPARATOR.get_include_path());
+	//echo "<p align=right>include_path='".get_include_path()."'</p>\n";
+}
+define('SEP',filesystem_separator());
+
+/**
+ * function to handle multilanguage support
+ *
+ */
+function lang($key,$vars=null)
+{
+	if(!is_array($vars))
+	{
+		$vars = func_get_args();
+		array_shift($vars);	// remove $key
+	}
+	return $GLOBALS['egw_setup']->translation->translate("$key", $vars);
+}
+
+if(file_exists(EGW_SERVER_ROOT.'/phpgwapi/setup/setup.inc.php'))
+{
+	include(EGW_SERVER_ROOT.'/phpgwapi/setup/setup.inc.php'); /* To set the current core version */
+	/* This will change to just use setup_info */
+	$GLOBALS['egw_info']['server']['versions']['current_header'] = $setup_info['phpgwapi']['versions']['current_header'];
+}
+else
+{
+	$GLOBALS['egw_info']['server']['versions']['phpgwapi'] = 'Undetected';
+}
+
+$GLOBALS['egw_info']['server']['app_images'] = 'templates/default/images';
+
+CreateObject('setup.setup',True,True);	// setup constuctor assigns itself to $GLOBALS['egw_setup'], doing it twice fails on some php4
+$GLOBALS['phpgw_setup'] =& $GLOBALS['egw_setup'];
